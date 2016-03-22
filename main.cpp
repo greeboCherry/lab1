42a9ee566107a363320eda307502e2a9777cdfff
greeboCherry <greebo4pl@gmail.com>
2016-03-23 00:14:26 +0100

    almost ready

--- main.cpp
+++ main.cpp
@@ -4,20 +4,54 @@
 
 char **charSquare(int n);
 void drawCharSquare(char **square, int n);
+//void createAndDrawCharSquare(int n); 
+//this appered here as effect of misunderstanding instruction for best mark, I'll lest this stay here for next generations browsing this repo
 
 int main(int argc, char **argv)
 {
-	int n;
-	drawCharSquare(charSquare(n), n);	
+	srand(atoi(argv[2]));
+	int n=atoi(argv[1]); //I'm not sure what's the point of making this variable if it's used only once but it was here since the begining so I guess it has rights to be here.
+	drawCharSquare(charSquare(n), n);
 }
+
 
 char **charSquare(int n)
 {
-	char **square;
+	char **square=new char*[n];
+	for(int i=0,j=0; i<n*n; i++)
+	{
+		square[i]=new char [n];
+		square[i][j]=rand()%26+'a'; 
+	}
+	//I'm so close of doing this in one instruction	:( / :)
+
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
-
+	for(int i=0; i<n*n; i++ )
+	{
+		if (i%n==0) 
+			printf(" line no.%i\n",i);
+		printf("%c ",**(square+i) );
+	}
 }
+
+/*
+void createAndDrawCharSquare(int n)
+//as explained above :)
+{
+	char **square=new char*[n];
+	for(int i=0,j;i<n;i++)
+	{
+		square[i]=new char [n];
+		for(j=0;j<n;j++)
+		{
+			square[i][j]=rand()%26+'a'; 
+			printf("%c ", square[i][j]);
+		}
+	printf("\n");
+	}
+}
+*/
